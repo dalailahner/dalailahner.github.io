a653eac96e2b3e5249923963a8000e23ce9dc537
dalailahner <59581086+dalailahner@users.noreply.github.com>
2024-04-29 17:07:04 +0200

    i hate img; i hate img; i hate img; i hate img;

--- scripts/index.js
+++ scripts/index.js
@@ -5,14 +5,13 @@ import bgCanvas from "./bgCanvas.js";
 ///////////////////////
 // GLOBAL VARIABLES: //
 let windowResizeTimeout;
-const bilderRow = { el: document.querySelector("#BilderRow"), scrollPos: 0, wasMoved: false };
 
 ///////////
 // INIT: //
 bgCanvas.init("#bgCanvas");
 
-/////////////
-// EVENTS: //
+////////////////////
+// GLOBAL EVENTS: //
 window.addEventListener("DOMContentLoaded", (event) => {
   bgCanvas.animate();
 });
@@ -29,38 +28,119 @@ window.addEventListener("resize", (event) => {
 
 /////////////////
 // PHOTOGRAPHY //
-bilderRow.el.addEventListener("pointerdown", (event) => {
-  bilderRow.wasMoved = false;
+const bilderRow = document.querySelector("#BilderRow");
+const slider = new Map().set("scrollPos", 0).set("downPos", 0).set("wasMoved", false);
+
+bilderRow.querySelectorAll("img").forEach((imgEl) => {
+  setSizeAttributes(imgEl);
+  imgEl.addEventListener("load", function BildLoad(event) {
+    setSizeAttributes(event.target);
+    event.target.removeEventListener("load", BildLoad);
+  });
 });
-bilderRow.el.addEventListener("pointermove", (event) => {
+
+bilderRow.addEventListener("pointerdown", (event) => {
   event.preventDefault();
-  if (event.pressure > 0.1) {
-    bilderRow.wasMoved = true;
-    bilderRow.scrollPos -= event.movementX;
-    window.requestAnimationFrame(() => {
-      setBilderScrollPos();
-    });
+  slider.set("downPos", event.x);
+  slider.set("wasMoved", false);
+});
+
+bilderRow.addEventListener("pointermove", (event) => {
+  event.preventDefault();
+  if (event.pressure > 0.1 && (event.x > slider.get("downPos") + 2 || event.x < slider.get("downPos") - 2)) {
+    let newPos = slider.get("scrollPos") - event.movementX;
+    slider.set("wasMoved", true);
+    if (bilderRow.querySelectorAll(".Bild.active").length > 0) {
+      const activeBilder = bilderRow.querySelectorAll(".Bild.active");
+      activeBilder.forEach((el) => {
+        switchImgResolution(el);
+        el.classList.remove("active");
+      });
+      newPos = getOffsetForElementCentering(activeBilder[0]);
+    }
+    setBilderScrollPos(newPos);
   }
 });
-bilderRow.el.addEventListener("pointerup", (event) => {
-  if (!bilderRow.wasMoved) {
-    // TODO: OPEN IMAGE IN MODAL
-  } else {
-    bilderRow.wasMoved = false;
+
+bilderRow.addEventListener("pointerup", (event) => {
+  if (slider.get("wasMoved")) {
+    slider.set("wasMoved", false);
     setBilderScrollPos();
+    return;
+  }
+  if (event.target.tagName === "IMG") {
+    switchImgResolution(event.target);
+    event.target.classList.toggle("active");
+    setBilderScrollPos(getOffsetForElementCentering(event.target));
   }
 });
-bilderRow.el.addEventListener("pointerleave", (event) => {
-  bilderRow.wasMoved = false;
+
+bilderRow.addEventListener("pointerleave", () => {
+  slider.set("wasMoved", false);
   setBilderScrollPos();
 });
 
-function setBilderScrollPos() {
-  bilderRow.el.style.transform = `translateX(-${bilderRow.scrollPos}px)`;
+function positionShelf() {
+  if (document.querySelector("#BilderScroll") && document.querySelector("#BilderRow")) {
+    document.querySelector("#BilderScroll").style.backgroundPositionY = `calc(50% + ${bilderRow.clientHeight * 0.535}px)`;
+  }
 }
 
-function positionShelf() {
-  document.querySelector("#BilderScroll").style.backgroundPositionY = `calc(50% + ${bilderRow.el.clientHeight * 0.535}px)`;
+function setSizeAttributes(element) {
+  if (element.getAttribute("width") > 0) {
+    element.setAttribute("width", element.clientWidth);
+  }
+  if (element.getAttribute("height") > 0) {
+    element.setAttribute("height", element.clientHeight);
+  }
+}
+
+function setBilderScrollPos(value) {
+  if (typeof value === "number") {
+    slider.set("scrollPos", value);
+  }
+  slider.set("maxPos", bilderRow.clientWidth - window.innerWidth);
+  if (slider.get("scrollPos") < 0) slider.set("scrollPos", 0);
+  if (slider.get("scrollPos") > slider.get("maxPos")) slider.set("scrollPos", slider.get("maxPos"));
+  bilderRow.style.transform = `translateX(-${slider.get("scrollPos")}px)`;
+}
+
+function getOffsetForElementCentering(targetEl) {
+  const parentRect = targetEl.parentElement.getBoundingClientRect();
+  let offset = slider.get("scrollPos") + (parentRect.x - (window.innerWidth - parentRect.width) / 2);
+  return offset;
+}
+
+function switchImgResolution(el) {
+  const windowAspectRatio = window.innerWidth / window.innerHeight;
+  const imgAspectRatio = el.clientWidth / el.clientHeight;
+  const currentImgUrl = el.src;
+  if (!el.classList.contains("active")) {
+    const newImage = new Image();
+    let newImgUrl = currentImgUrl.replace("/S/", "/L/");
+    newImage.src = newImgUrl;
+    if (windowAspectRatio > imgAspectRatio) {
+      const height = window.innerHeight * 0.8;
+      const width = height * imgAspectRatio;
+      el.style = `width: ${width}px;max-width: ${width}px;height: ${height}px;max-height: ${height}px`;
+    } else {
+      const width = window.innerWidth * 0.9;
+      const height = width / imgAspectRatio;
+      el.style = `width: ${width}px;max-width: ${width}px;height: ${height}px;max-height: ${height}px`;
+    }
+    el.addEventListener("transitionend", function swapImg() {
+      el.src = newImgUrl;
+      el.removeEventListener("transitionend", swapImg);
+    });
+    return;
+  }
+  if (el.classList.contains("active")) {
+    el.src = currentImgUrl.replace("/L/", "/S/");
+    const width = el.getAttribute("width");
+    const height = el.getAttribute("height");
+    el.style = `width: ${width}px;max-width: ${width}px;height: ${height}px;max-height: ${height}px`;
+    return;
+  }
 }
 
 ///////////////////////
